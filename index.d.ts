24aabb1bc294c284826c3b23fc6808370d24c8a9
Alexander Schill <alexander@schillnet.de>
2025-11-11 17:01:09 +0100

    Fix broken type exports

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,23 @@
-export { HomeAssistant } from './frontend/src/types';
+export type { Constructor, HomeAssistant } from './frontend/src/types';
+export type { Condition } from './frontend/src/panels/lovelace/common/validate-condition';
 
-export { Condition } from './frontend/src/panels/lovelace/common/validate-condition';
-export { Constructor,  } from './frontend/src/types';
+export type * from "./frontend/src/data/lovelace_custom_cards";
 
-export * from "./frontend/src/panels/lovelace/types";
-export * from "./frontend/src/data/lovelace/config/card";
-export * from "./frontend/src/data/lovelace_custom_cards";
+export type * from "./frontend/src/panels/lovelace/types";
+export type * from "./frontend/src/panels/lovelace/cards/types";
+export type * from "./frontend/src/panels/lovelace/card-features/types";
+export type * from "./frontend/src/panels/lovelace/elements/types";
+export type * from "./frontend/src/panels/lovelace/badges/types";
 
-import { CustomCardsWindow } from './frontend/src/data/lovelace_custom_cards';
+export type * from "./frontend/src/data/lovelace/config/action";
+export type * from "./frontend/src/data/lovelace/config/badge";
+export type * from "./frontend/src/data/lovelace/config/card";
+export type * from "./frontend/src/data/lovelace/config/section";
+export type * from "./frontend/src/data/lovelace/config/strategy";
+export type * from "./frontend/src/data/lovelace/config/view";
+export type * from "./frontend/src/data/lovelace/config/types";
+
+import type { CustomCardsWindow } from './frontend/src/data/lovelace_custom_cards';
 export type CustomCardHelpers = typeof import('./frontend/src/panels/lovelace/custom-card-helpers')
 
 declare global {
